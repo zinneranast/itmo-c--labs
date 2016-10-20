6882dc1e08b22d9e9c232e29b4a8b5dad7498421
Anastasia Zinner <Anastasia Zinner>
2016-10-20 08:27:43 +0300

    change

--- omp/omp/main.cpp
+++ omp/omp/main.cpp
@@ -1,50 +1,64 @@
 #include "stdafx.h"
 #include <omp.h>
 
-void ReadFile(const char *fileName) {
-	float **matrixA;
+vector<vector<int>> ReadFile(const char *fileName) {
 	int n, m;
+	ifstream file;
 
-	FILE *file = fopen(fileName, "r");
-	if (file == NULL)
-		printf("File not found\nPress any key to end");
-	else {
-		fscanf(file, "%d %d", &n, &m);
-		matrixA = new float*[n];
-		for (int i = 0; i < n; i++){
-			matrixA[i] = new float[m];
-		}
+	try {
+		file.open(fileName);
+		file >> n >> m;
+		vector<vector<int>> matrix(n, vector<int>(m));
+
 		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++){
-				fscanf(file, "%f", &matrixA[i][j]);
-			}
-		fclose(file);
-
-		for (int i = 0; i < n; i++){
 			for (int j = 0; j < m; j++)
-				printf("%f ", matrixA[i][j]);
-			printf("\n");
-		}
+				file >> matrix[i][j];		
+
+		file.close();
+		return matrix;
+	}
+	catch (exception &e) {
+		printf("Error: File %s does not exist\n", fileName);
 	}
 }
 
-long long fibonachi(long int n){
-	if (n == 0 || n == 1)
-		return 0;
-	return fibonachi(n - 1) + fibonachi(n - 2);
+vector<vector<int>> MultiplyMatrixes(vector<vector<int>> matrixA, vector<vector<int>> matrixB){
+	vector<vector<int>> matrixC(matrixA.size(), vector<int>(matrixB[0].size()));
+
+	for (int i = 0; i < matrixA.size(); i++)
+		for (int j = 0; j < matrixB[0].size(); j++)
+			for (int k = 0; k < matrixA[0].size(); k++)
+				matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+
+	return matrixC;
 }
 
 int _tmain(int argc, _TCHAR* argv[]) {
 	time_t begin, end;
-
 	time(&begin);
 
-	const char *fileName = "C:\\Sources\\itmo-c++labs\\omp\\files\\matrixA.in";
-	ReadFile(fileName);
+	const char *fileName;
+	fileName = "C:\\Sources\\itmo-c++labs\\omp\\files\\matrixA.in";
+	vector<vector<int>> matrixA = ReadFile(fileName);
+	fileName = "C:\\Sources\\itmo-c++labs\\omp\\files\\matrixB.in";
+	vector<vector<int>> matrixB = ReadFile(fileName);
+
+	printf("%d\n%d\n", matrixA.size(), matrixA[0].size());
+	printf("%d\n%d\n", matrixB.size(), matrixB[0].size());
+
+	if (matrixA[0].size() == matrixB.size()){
+		vector<vector<int>> matrixC;
+		matrixC = MultiplyMatrixes(matrixA, matrixB);
+
+		for (int i = 0; i < matrixC.size(); i++){
+			for (int j = 0; j < matrixC[0].size(); j++)
+				cout << matrixC[i][j] << " ";
+			cout << endl;
+		}
+	}
 
 	time(&end);
-	printf("\n%f\n", difftime(end, begin));
-
+	printf("%.2f\n", difftime(end, begin));
 	system("pause");
 
 	return 0;
--- omp/omp/stdafx.h
+++ omp/omp/stdafx.h
@@ -3,4 +3,11 @@
 #include <stdlib.h>
 #include <TCHAR.h>
 #include <time.h>
-#include <string>
+#include <exception>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#pragma warning(disable : 4101)
+#pragma warning(disable : 4018)
+#pragma warning(disable : 4715)
+using namespace std;
